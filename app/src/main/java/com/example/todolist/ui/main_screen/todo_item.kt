2ac1bc5258446634f9dda9b88e76d937344694f3
BRBX <egorka2005sautkin@gmail.com>
2023-12-06 22:53:15 +0300

    todo item is almost done

--- app/src/main/java/com/example/todolist/ui/main_screen/todo_item.kt
+++ app/src/main/java/com/example/todolist/ui/main_screen/todo_item.kt
@@ -1,18 +1,29 @@
 package com.example.todolist.ui.main_screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -29,7 +40,7 @@ fun TodoItem() {
         Font(R.font.ubuntu_medium, FontWeight.Medium),
     )
 
-    Box(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight(0.2f)
@@ -45,15 +56,61 @@ fun TodoItem() {
             contentAlignment = Alignment.Center
         ) {
             Text(
-                text = "27 SEPT",
+                text = "6 DE",
                 color = Color.White,
                 fontWeight = FontWeight.Bold,
                 fontFamily = fontFamily,
                 fontSize = 15.sp,
                 modifier = Modifier
                     .rotate(-90f)
-                    .background(Color.Red),
+                    .alpha(0.7f)
             )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Complete wireframes for Power-x",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = fontFamily,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .alpha(0.7f)
+                )
+                Text(
+                    text = "12:00 - 17:30",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = fontFamily,
+                    fontSize = 25.sp,
+                    modifier = Modifier
+                        .alpha(0.7f)
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .border(3.dp, Color(0xFF8B618F), CircleShape)
+                    .clip(CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.not_complited_todo_icon),
+                    contentDescription = "Not completed todo icon",
+                    tint = Color(0xFF8B618F),
+                    modifier = Modifier
+                        .size(12.dp)
+                )
+            }
         }
     }
 }
